357529c1cd3fa59b0f6058b78658987704d6d839
Sakhile Khuzwayo <St10340742@rcconnect.edu.za>
2024-02-12 19:53:46 +0200

    added comments for explanation of logic

--- selection sort/SelectionSort.java
+++ selection sort/SelectionSort.java
@@ -1,10 +1,14 @@
 //Selection sort
-/*selects minmum, swaps using temp the value where the minimum should be
+/*selects minmum, swaps using temp the value
+ where the minimum should be
 and pushes set to the left*/
 
-/**
- * Main
- */
+/*searches through an array and keeps 
+track of the minimum value during each iteration.
+At the end of each iteration, we swap variable
+*/
+
+
 public class SelectionSort {
 
     public static void main(String[] args) {
@@ -14,15 +18,21 @@ public class SelectionSort {
         selectionSort(array);
         
         for (int i : array) {
-            System.out.println(i);
+            System.out.println(i); //print elements
         }
     }
 
+    //pass array as argument    
     private static void selectionSort(int[] array) {
-    
+        
+
         for (int i = 0; i< array.length -1; i++ ) {
             
-            int min = i;
+            int min = i; //keep track of current minimum
+
+            /*to check if minimum of array
+             is greater than array at index of j
+            */
 
             for (int j = i +1; j< array.length; j++) {
                 if(array[min] > array[j]){
@@ -30,6 +40,7 @@ public class SelectionSort {
                 }
             }
 
+            //swap minimum with array element [i]
             int temp = array[i];
             array[i] = array[min];
             array[min] = temp;
